4cf9ca24542ba17025dd8884046a6ad464e743f7
fixel92 <volkov-site@mail.ru>
2021-04-17 11:54:01 +0300

    my_output.html was upgraded, text and date of post or comment were added in collecting data

--- lib/senders/html_sender.rb
+++ lib/senders/html_sender.rb
@@ -1,20 +1,66 @@
 class HtmlSender
   def send(data)
-    File.write('my_output.html', repare_data(data), mode: 'w')
+    File.write('my_output.html', html_body(data), mode: 'w')
   end
 
-  def repare_data(data)
-    text = data.values.map do |i|
-      "<li><a target='_blank' href='#{URI.extract(i[:url])}'>#{i[:type]} - #{i[:url]} - #{i[:keywords]}\n</a></li>"
+  private
+
+  def prepare_keywords_labels(keywords)
+    keywords.map do |keyword|
+      "<span class='ui teal tiny label'>#{keyword}</span>"
     end.join(' ')
-
-    list = "<ul style='list-style-type: none'>#{text}</ul>"
-
-    '<html><style>a{text-decoration: none;}</style><body>' \
-               "<h1>#{title}</h1>#{list}</body></html>"
   end
 
-  def title
-    'Ссылки на посты и комментарии по ключевым словам:'
+  def prepare_rows(data)
+    data.values.map do |i|
+      %(
+      <tr>
+        <td>#{i[:type]}</td>
+        <td><small>#{i[:text]}</small></td>
+        <td><b>#{i[:date]}</b></td>
+        <td><small><a href="#{i[:url]}" target="_blank">Перейти</a></small></td>
+        <td><span class="ui teal tiny label">#{prepare_keywords_labels(i[:keywords])}</td>
+      </tr>
+    )
+    end.join(' ')
+  end
+
+  def html_body(data)
+    %(
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Результаты парсинга</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+      </head>
+      <body>
+      <style type="text/css">
+        A:visited {
+          color: #80007a;
+        }
+      </style>
+        <h2 class="ui center aligned header">Ссылки на посты и комментарии по ключевым словам:</h2>
+        <table class="ui celled padded table">
+          <thead>
+            <tr>
+              <th>Тип записи</th>
+              <th>Текст</th>
+              <th>Дата</th>
+              <th>Ссылка</th>
+              <th>Ключевые слова</th>
+            </tr>
+          </thead>
+          <tbody>
+            #{prepare_rows(data)}
+          </tbody>
+        </table>
+      </body>
+      </html>
+    )
   end
 end
